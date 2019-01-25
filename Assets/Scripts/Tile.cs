da03ef8a0925bf15be089fad30a39ea82f10627d
yosubis <yosubis@no_mail.com>
2019-01-25 19:49:23 +0200

    Created base Tile class.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+	[SerializeField] protected Transform _playerPositionTransform;
+	
+	public Vector3 playerMarker{
+        get{
+            return _playerPositionTransform.position;
+        }
+    }
+}
