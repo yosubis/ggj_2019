42de8c41a24a70ffe37e08abf2af33da87e1c938
yosubis <yosubis@no_mail.com>
2019-01-25 19:49:48 +0200

    Created docking station.

--- Assets/Scripts/Station.cs
+++ Assets/Scripts/Station.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Station : MonoBehaviour
+public class Station : Tile
 {
     private static List<Station> _allStations = new List<Station>();
-    [SerializeField] private Transform _spawnPoint;
 
     void Awake(){
         _allStations.Add(this);
     }
+
+    public void Dock(Battery battery){
+		battery.StartCharge();
+	}
+
+	public void Undock(Battery battery){
+		battery.EndCharge();
+	}
 }
